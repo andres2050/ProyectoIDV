c8bc5ad74056fa467dfc3f153033eb79bfdd0a49
camiloAbv <abv.camilo@gmail.com>
2020-10-27 01:37:29 -0500

    evento cerrar puertas
    implementando cambiar la propiedad de una tile cuando se activa un evento

--- scripts/Event.gd
+++ scripts/Event.gd
@@ -3,10 +3,16 @@ extends Node2D
 export var canSpawn = true 
 export var spawnTime = 1
 export(Array, NodePath) var spawners
+export(Array, Vector2) var doors
 
+
+onready var scenario = get_node("/root/Main/Scenario")
 func Start_Event():
 	if canSpawn: 
 		canSpawn = false
 		for i in range(spawners.size()):
 			get_node(spawners[i]).canSpawn = true
 			get_node(spawners[i]).spawnRate = spawnTime
+			
+		for i in range(doors.size()):
+			scenario.set_cellv(doors[i],4)
