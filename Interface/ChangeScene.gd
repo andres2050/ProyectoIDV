f0f45de545f822465da4e8f8796afef119e6a53a
camiloAbv <abv.camilo@gmail.com>
2020-11-08 15:24:20 -0500

    pequeños arreglos

--- Interface/ChangeScene.gd
+++ Interface/ChangeScene.gd
@@ -3,15 +3,20 @@ extends Button
 export(String, "main.tscn","Start_Menu.tscn") var scene
  
 export var animation_path :NodePath
-onready var animation = get_node(animation_path)
 var animation_duration
+var animation
+func _ready():
+	if (animation_path):
+		animation = get_node(animation_path)
+		
 func _on_Button_pressed():
-	animation_duration = animation.current_animation_length
 	
 	if get_name() == "Exit":
 		get_tree().quit()
 	else:
-		if animation != null:
+		if animation:
+			animation_duration = animation.current_animation_length
+			
 			animation.play_backwards("Fading")
 			yield(get_tree().create_timer(animation_duration),"timeout")
 		get_tree().paused = false
