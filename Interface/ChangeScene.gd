67fff62a3df0b72b9be1ade58d6eb4a012767349
Andres Herrera <andres.2050@hotmail.com>
2020-11-27 13:46:01 -0500

    Add introduction video  for level 1

--- Interface/ChangeScene.gd
+++ Interface/ChangeScene.gd
@@ -1,6 +1,6 @@
 extends Button
 
-export(String, "Test_level","Start_Menu","Level_0", "Level_1" ) var scene
+export(String, "Test_level","Start_Menu","Level_0", "Level_1", "Introduction_Level_1" ) var scene
 export(String, "Fading", "DeadFading", "DeadScreen") var animation
 
 export var animation_path :NodePath
@@ -18,7 +18,11 @@ func _on_Button_pressed():
 			animationPlayer.play_backwards(animation)
 			
 			animation_duration = animationPlayer.current_animation_length
-			yield(get_tree().create_timer(animation_duration),"timeout")
+			yield(get_tree().create_timer(animation_duration), "timeout")
 		get_tree().paused = false
 		if get_tree().change_scene("res://scenes/" + scene + ".tscn") != OK:
 			print("An unexpected error occured when trying to switch to the Readme scene")
+
+
+func _on_StartButton_pressed():
+	pass # Replace with function body.
