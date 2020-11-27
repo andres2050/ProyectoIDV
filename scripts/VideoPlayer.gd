d33dc617596b04c644156d0a9aafe00ebc20eb89
Andres Herrera <andres.2050@hotmail.com>
2020-11-27 15:32:51 -0500

    add static enemies

--- scripts/VideoPlayer.gd
+++ scripts/VideoPlayer.gd
@@ -1,19 +1,8 @@
 extends VideoPlayer
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	play()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
 func _on_VideoPlayer_finished():
-	get_tree().change_scene("res://scenes/Level_1.tscn")
+	if get_tree().change_scene("res://scenes/Level_1.tscn") != OK:
+		print("An unexpected error occured when trying to switch to the Readme scene")
---
+++ scripts/static.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export var health = 15
+export var damage = 0
+
+func _process(_delta):
+	if health <= 0:
+		die()
+
+func damage_health(incoming_damage, knockbackForce):
+	health=health - incoming_damage
+
+func die():
+	queue_free()
