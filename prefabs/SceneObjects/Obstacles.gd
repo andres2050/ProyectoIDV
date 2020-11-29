dfdb713d4cff443e6f613e881983bccc98501275
camiloAbv <abv.camilo@gmail.com>
2020-11-29 07:18:25 -0500

    poner obstaculos delante del jugador tranasparentes

---
+++ prefabs/SceneObjects/Obstacles.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+var smooth_fade = 0.001
+var entered = false
+func _on_Area2D_body_entered(body):
+	if body.is_in_group("player"):
+		entered = true
+		while(modulate.a > 0.4 and entered):
+			yield(get_tree().create_timer(smooth_fade),"timeout")
+			modulate.a -= 0.03
+		
+
+
+
+func _on_Area2D_body_exited(body):
+	if body.is_in_group("player"):
+		entered = false
+		while(modulate.a < 1 and !entered):
+			yield(get_tree().create_timer(smooth_fade),"timeout")
+			modulate.a += 0.01
