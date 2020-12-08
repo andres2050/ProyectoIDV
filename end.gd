97ca9cdd6b56c977f7884e5899bf0f6e4566a0c8
camiloAbv <abv.camilo@gmail.com>
2020-12-08 18:07:07 -0500

    arreglando_final

--- end.gd
+++ end.gd
@@ -5,5 +5,13 @@ func end():
 	get_node("Control").visible = true
 	animation_player.play("show_thanks")
 	yield(get_tree().create_timer(animation_player.current_animation_length),"timeout")
-	if get_tree().change_scene("res://scenes/Start_Menu.tscn") != OK:
-		print("not ok")
+	
+	
+func change_scene_manually():
+	var next_scene = load("res://scenes/Start_Menu.tscn").instance()
+	var main_node = self
+	while(main_node.get_parent() != get_tree().get_root()):
+		main_node = main_node.get_parent()
+	
+	get_tree().get_root().add_child(next_scene)
+	main_node.queue_free()
